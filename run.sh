1693d95b03f16b7e26dd4e1e55412881042d64df
Patrick Lee <31744877+patricklee2@users.noreply.github.com>
2019-07-01 13:53:13 -0700

    init

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+docker volume create ssh-vol
+
+docker build -t ssh-cont ssh
+
+docker build -t custom-image nossh
+
+docker run -d \
+	--name ssh-cont \
+	--mount source=ssh,target=/etc/ssh \
+	ssh-cont
+
+docker rm ssh-cont
+
+docker run -d \
+    --name cont \
+    --mount source=ssh,/target/etc/ssh \
+    custom-image
+    
