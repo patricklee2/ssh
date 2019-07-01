5a5e32e50775115d6c16e58a3bf0297d19ebf0fb
Patrick Lee <31744877+patricklee2@users.noreply.github.com>
2019-07-01 16:31:57 -0700

    init

--- run.sh
+++ run.sh
@@ -7,22 +7,28 @@ docker rm cont
 docker rmi ssh-image
 docker rmi custom-image
 
-docker volume create ssh-vol
+docker volume create my-vol
 
-docker build --no-cache -t ssh-image ssh
+docker build  -t a-image ssh
 
-docker build --no-cache -t custom-image nossh
-
-docker run -d \
-	--name ssh-cont \
-	--mount source=ssh,target=/etc/ssh \
-	ssh-image
-
-docker stop ssh-cont
-docker rm ssh-cont
+docker build  -t b-image nossh
 
 docker run -d \
-    --name cont \
-    --mount source=ssh,target=/etc/ssh \
-    custom-image
+	--name a-cont \
+	--mount source=my-vol,target=/home/a \
+	a-image
 
+docker exec -it a-cont bash
+
+docker stop a-cont
+docker rm a-cont
+
+
+docker stop b-cont
+docker rm b-cont
+docker run -d \
+    --name b-cont \
+    --mount type=volume,source=my-vol,target=/home/a \
+    b-image
+
+docker exec -it b-cont bash
