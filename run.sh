aba29f79174e98db89c131231a53f6f36489921c
Patrick Lee <31744877+patricklee2@users.noreply.github.com>
2019-07-03 13:23:49 -0700

    init

--- run.sh
+++ run.sh
@@ -11,24 +11,38 @@ docker volume create my-vol
 
 docker build  -t a-image ssh
 
-docker build  -t b-image nossh
-
 docker run -d \
 	--name a-cont \
-	--mount source=my-vol,target=/home/a \
+	--mount source=my-vol,target=/etc/ssh \
 	a-image
 
-docker exec -it a-cont bash
+# docker exec -it a-cont bash
 
 docker stop a-cont
 docker rm a-cont
 
-
+docker build  -t b-image nossh
 docker stop b-cont
 docker rm b-cont
-docker run -d \
+docker run -d -P \
     --name b-cont \
-    --mount type=volume,source=my-vol,target=/home/a \
+    --mount type=volume,source=my-vol,target=/etc/ssh \
     b-image
 
 docker exec -it b-cont bash
+
+
+#apt-get install -y openssh-server
+apt-get update -y
+apt-get upgrade -y openssl
+cd /etc/ssh
+wget -c https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-8.0p1.tar.gz
+tar -xzf openssh-8.0p1.tar.gz
+cd openssh-8.0p1/
+./configure --prefix=/etc/ssh --sysconfdir=/etc/ssh --with-pid-dir=/etc/ssh
+make
+make install 
+cd ..
+
+mkdir -p /var/run/sshd
+/etc/ssh/sshd
---
+++ ssh/start_sshd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+groupadd sshd
+useradd -g sshd -d / sshd
+/etc/ssh/sbin/sshd
