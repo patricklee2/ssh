407141748204c95c076f173426ff8bdb0f3b81fd
Patrick Lee <31744877+patricklee2@users.noreply.github.com>
2019-07-03 14:31:32 -0700

    fixes

---
+++ make-ssh-volume.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker volume delete ssh-vol
+docker volume create ssh-vol
+
+docker stop make-ssh-vol-cont
+docker rm make-ssh-vol-cont
+
+docker build -t make-ssh-vol-image make-ssh-vol
+docker run -d -P \
+    --name make-ssh-vol-cont \
+    --mount type=volume,source=ssh-vol,target=/etc/ssh \
+    b-image
--- run.sh
+++ run.sh
@@ -1,48 +1,14 @@
 #!/bin/bash
 
-docker stop ssh-cont
-docker rm ssh-cont
-docker stop cont
-docker rm cont
-docker rmi ssh-image
-docker rmi custom-image
-
-docker volume create my-vol
-
-docker build  -t a-image ssh
-
-docker run -d \
-	--name a-cont \
-	--mount source=my-vol,target=/etc/ssh \
-	a-image
-
-# docker exec -it a-cont bash
-
 docker stop a-cont
 docker rm a-cont
 
-docker build  -t b-image nossh
-docker stop b-cont
-docker rm b-cont
+docker build  -t a-image nossh
+docker stop a-cont
+docker rm a-cont
 docker run -d -P \
     --name b-cont \
     --mount type=volume,source=my-vol,target=/etc/ssh \
     b-image
 
-docker exec -it b-cont bash
-
-
-#apt-get install -y openssh-server
-apt-get update -y
-apt-get upgrade -y openssl
-cd /etc/ssh
-wget -c https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-8.0p1.tar.gz
-tar -xzf openssh-8.0p1.tar.gz
-cd openssh-8.0p1/
-./configure --prefix=/etc/ssh --sysconfdir=/etc/ssh --with-pid-dir=/etc/ssh
-make
-make install 
-cd ..
-
-mkdir -p /var/run/sshd
-/etc/ssh/sshd
+docker exec -it b-cont /etc/ssh/start_sshd.sh
